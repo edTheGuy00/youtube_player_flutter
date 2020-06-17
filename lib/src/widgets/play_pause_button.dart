24f6e0e924b67a8101f6a7d76427a483ebecc996
Ryo <fujimoto.ryoutaro@gmail.com>
2020-06-17 08:46:25 +0200

    style

--- lib/src/widgets/play_pause_button.dart
+++ lib/src/widgets/play_pause_button.dart
@@ -84,7 +84,7 @@ class _PlayPauseButtonState extends State<PlayPauseButton>
             child: AnimatedIcon(
               icon: AnimatedIcons.play_pause,
               progress: _animController.view,
-              color: Colors.white,
+              color: Colors.grey[300],
               size: widget.iconSize,
             ),
           ),
